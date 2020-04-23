7c89cf7ecbe769d91453064756dcd2c2c175634d
Lee, Hosoo <Hosoo.Lee@arithnea.de>
2020-04-23 23:47:51 +0200

    first commit

---
+++ img_crawling.py
@@ -0,0 +1,27 @@
+from urllib.request import urlopen
+from urllib.parse import quote_plus
+from bs4 import BeautifulSoup
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+baseUrl = 'https://search.naver.com/search.naver?where=image&sm=tab_jum&query='
+plusUrl = input('Please write a Keyword : ')
+url = baseUrl + quote_plus(plusUrl)
+
+html = urlopen(url).read()
+soup = BeautifulSoup(html, 'html.parser')
+img = soup.findAll(class_='_img')
+
+
+n = 1
+for i in img:
+    imgUrl = i['data-source']
+    with urlopen(imgUrl) as f:
+        with open('./img/' + plusUrl + str(n) + '.jpg', 'wb') as h:
+            img = f.read()
+            h.write(img)
+    n += 1
+    print(imgUrl)
+
+print('Download Complete')
